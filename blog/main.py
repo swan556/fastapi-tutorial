c0359d8108467566589779ff62f6840390017e34
Swan <swanand556@gmail.com>
2025-10-19 11:27:59 +0530

    connected db to input via fastapi page

--- blog/main.py
+++ blog/main.py
@@ -1,15 +1,23 @@
 from fastapi import FastAPI, Depends
 from blog.schemas import Blog
 from blog import models
-from .database import engine
+from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 def get_db():
-    db = Session
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 models.Base.metadata.create_all(engine)
 
 @app.post('/blog')
 def create(request: Blog, db: Session = Depends(get_db)):
-    return request
+    new_blog = models.Blog(title=request.title, body = request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
